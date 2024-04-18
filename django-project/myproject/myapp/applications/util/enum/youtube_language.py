dc373528542411fe4cf2791cf5d0f536e2d502ca
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-19 00:21:46 +0900

    言語コード動作確認

--- django-project/myproject/myapp/applications/util/enum/youtube_language.py
+++ django-project/myproject/myapp/applications/util/enum/youtube_language.py
@@ -7,3 +7,22 @@ class YouTubeLanguage(Enum):
     KOREAN = "ko"  # 韓国語
     CHINESE = "zh"  # 中国語
     INDONESIAN = "id"  # インドネシア語
+
+
+# Enumの値を入れる処理
+def set_language(language_code):
+    for lang in YouTubeLanguage:
+        if lang.value == language_code:
+            return lang
+    raise ValueError("Invalid language code")
+
+
+# 使用例
+code = "ja"
+enum_obj = set_language(code)
+print(enum_obj.value)  # 出力: ja
+
+# Enumを直接指定する
+korean_enum = YouTubeLanguage.KOREAN
+
+print(korean_enum.value)  # 出力: ko
