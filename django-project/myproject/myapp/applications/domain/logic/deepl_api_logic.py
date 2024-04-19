d9209b8159a5bec1dae3a8f432f6f27e006a1599
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-19 20:23:28 +0900

    deepl api作成

---
+++ django-project/myproject/myapp/applications/domain/logic/deepl_api_logic.py
@@ -0,0 +1,69 @@
+import unittest
+
+import deepl
+
+from myapp.applications.util.code.deepl_language import DeepLLanguage
+from myproject.settings.base import DEEPL_API_KEY
+
+
+class DeepLApiLogic:
+    def __init__(self):
+        # DeepLのAPIキーを読み込む
+        self.api_key = DEEPL_API_KEY
+
+    def translation(self, word, lang):
+        translator = deepl.Translator(self.api_key)
+        result = translator.translate_text(word, target_lang=lang)
+        return result
+
+
+class TestDeepLApiLogic(unittest.TestCase):
+    def setUp(self):
+        # 翻訳する単語をセットアップ
+        self.word = "こんにちは。私は英語の勉強がしたいです。"
+
+    def test_translation_japanese(self):
+        # 日本語への翻訳をテスト
+        lang = DeepLLanguage.JAPANESE
+        deepl_logic = DeepLApiLogic()
+        translated_word = deepl_logic.translation(self.word, lang.value)
+        print(f"{lang.name}への翻訳: {translated_word}")
+
+    def test_translation_english_gb(self):
+        # 英語 (イギリス) への翻訳をテスト
+        lang = DeepLLanguage.ENGLISH_GB
+        deepl_logic = DeepLApiLogic()
+        translated_word = deepl_logic.translation(self.word, lang.value)
+        print(f"{lang.name}への翻訳: {translated_word}")
+
+    def test_translation_english_us(self):
+        # 英語 (アメリカ) への翻訳をテスト
+        lang = DeepLLanguage.ENGLISH_US
+        deepl_logic = DeepLApiLogic()
+        translated_word = deepl_logic.translation(self.word, lang.value)
+        print(f"{lang.name}への翻訳: {translated_word}")
+
+    def test_translation_korean(self):
+        # 韓国語への翻訳をテスト
+        lang = DeepLLanguage.KOREAN
+        deepl_logic = DeepLApiLogic()
+        translated_word = deepl_logic.translation(self.word, lang.value)
+        print(f"{lang.name}への翻訳: {translated_word}")
+
+    def test_translation_chinese(self):
+        # 中国語への翻訳をテスト
+        lang = DeepLLanguage.CHINESE
+        deepl_logic = DeepLApiLogic()
+        translated_word = deepl_logic.translation(self.word, lang.value)
+        print(f"{lang.name}への翻訳: {translated_word}")
+
+    def test_translation_indonesian(self):
+        # インドネシア語への翻訳をテスト
+        lang = DeepLLanguage.INDONESIAN
+        deepl_logic = DeepLApiLogic()
+        translated_word = deepl_logic.translation(self.word, lang.value)
+        print(f"{lang.name}への翻訳: {translated_word}")
+
+
+# if __name__ == '__main__':
+#     unittest.main()
---
+++ django-project/myproject/myapp/applications/util/code/deepl_language.py
@@ -0,0 +1,25 @@
+from enum import Enum
+import unittest
+
+
+class DeepLLanguage(Enum):
+    JAPANESE = "ja"  # 日本語
+    ENGLISH_GB = "en-GB"  # 英語 (イギリス)
+    ENGLISH_US = "en-US"  # 英語 (アメリカ)
+    KOREAN = "ko"  # 韓国語
+    CHINESE = "zh"  # 中国語
+    INDONESIAN = "id"  # インドネシア語
+
+
+class TestDeepLLanguage(unittest.TestCase):
+    def test_enum_values(self):
+        self.assertEqual(DeepLLanguage.JAPANESE.value, "ja")
+        self.assertEqual(DeepLLanguage.ENGLISH_GB.value, "en-GB")
+        self.assertEqual(DeepLLanguage.ENGLISH_US.value, "en-US")
+        self.assertEqual(DeepLLanguage.KOREAN.value, "ko")
+        self.assertEqual(DeepLLanguage.CHINESE.value, "zh")
+        self.assertEqual(DeepLLanguage.INDONESIAN.value, "id")
+
+
+if __name__ == '__main__':
+    unittest.main()
