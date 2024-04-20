3ace805087a5cc8ddb21c05cea10ba3560a93f41
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-20 14:05:32 +0900

    修正

--- django-project/myproject/myapp/applications/domain/logic/youtube_download_logic.py
+++ django-project/myproject/myapp/applications/domain/logic/youtube_download_logic.py
@@ -50,12 +50,12 @@ class YouTubeDownloadLogic:
             logging.error('ビデオのダウンロード中にエラーが発生しました:', exc_info=True)
         logging.info('download_video_mp4 end')
 
-    def download_subtitle_vtt(self, video_id: str, lang: str, output_path: str, encoding='utf-8') -> str:
+    def download_subtitle_vtt(self, video_id: str, lang: YouTubeLanguage, output_path: str, encoding='utf-8') -> str:
         """指定したYouTubeビデオの字幕ファイルをダウンロードする
 
         Args:
             video_id (str): YouTubeのビデオID
-            lang (str): 字幕の言語コード（例: 'en'）
+            lang (YouTubeLanguage): 字幕の言語
             output_path (str): 保存先のファイルパス
             encoding (str, optional): 字幕ファイルのエンコーディング（デフォルトは 'utf-8'）
 
@@ -68,7 +68,7 @@ class YouTubeDownloadLogic:
                 'writesubtitles': True,  # 字幕を書き出す
                 'writeautomaticsub': True,  # 自動生成字幕を書き出す
                 'skip_download': True,  # ダウンロードをスキップ
-                'subtitleslangs': [lang],  # 字幕の言語を指定
+                'subtitleslangs': [lang.value],  # 字幕の言語を指定
                 'outtmpl': f'{output_path}.%(ext)s',  # 出力パスのテンプレート
             }
 
@@ -77,7 +77,7 @@ class YouTubeDownloadLogic:
                 ydl.download(['https://youtu.be/' + video_id])
 
             # ダウンロードしたファイルのパス
-            file_path = f'{output_path}.{lang}.vtt'
+            file_path = f'{output_path}.{lang.value}.vtt'
 
             # ファイルが存在するか確認して、内容を取得
             if os.path.exists(file_path):
@@ -99,7 +99,7 @@ class TestYouTubeDownloadLogic(unittest.TestCase):
     def test_download_subtitle_vtt(self):
         youtube_download_logic = YouTubeDownloadLogic()
         subtitles_content = youtube_download_logic.download_subtitle_vtt(TEST_YOUTUBE_VIDEO_ID,
-                                                                         YouTubeLanguage.JAPANESE.value,
+                                                                         YouTubeLanguage.JAPANESE,
                                                                          TEST_DIR + TEST_YOUTUBE_VIDEO_ID)
         print(subtitles_content)
 
---
+++ django-project/myproject/myapp/applications/util/code/character_code.py
@@ -0,0 +1,55 @@
+from enum import Enum
+import unittest
+
+
+class CharacterCode(Enum):
+    KS_X_1001 = "KS X 1001"  # 韓国語文字コード
+    SHIFT_JIS = "Shift-JIS"  # 日本語文字コード
+    GB2312 = "GB2312"  # 中国語文字コード
+    UTF8 = "UTF-8"  # 英語文字コード (UTF-8は英語を含む多くの言語で使用される)
+    ISO_8859_1 = "ISO-8859-1"  # インドネシア語文字コード (ISO-8859-1は西ヨーロッパ言語で広く使用されています)
+    # 他の文字コードを追加する場合はここに追加する
+
+
+# TODO:文字コードについて理解できていない
+class TestStringEncoding(unittest.TestCase):
+    def encode_as(self, string_to_encode, code):
+        return string_to_encode.encode(code.value)
+
+    def decode_as(self, encoded_string, code):
+        return encoded_string.decode(code.value)
+
+    def test_encode_decode_korean(self):
+        string_to_encode = "안녕하세요, 세계!"  # 韓国語文字列
+        encoded_string = self.encode_as(string_to_encode, CharacterCode.UTF8)
+        decoded_string = self.decode_as(encoded_string, CharacterCode.UTF8)
+        self.assertEqual(decoded_string, string_to_encode)
+
+    def test_encode_decode_japanese(self):
+        string_to_encode = "こんにちは、世界！"  # 日本語文字列
+        encoded_string = self.encode_as(string_to_encode, CharacterCode.UTF8)
+        decoded_string = self.decode_as(encoded_string, CharacterCode.UTF8)
+        self.assertEqual(decoded_string, string_to_encode)
+
+    def test_encode_decode_chinese(self):
+        string_to_encode = "你好，世界！"  # 中国語文字列
+        # 例えば你好，我想学习英语。我想学习英语。のフォントがおかしいのはどうしたらいい。
+        encoded_string = self.encode_as(string_to_encode, CharacterCode.UTF8)
+        decoded_string = self.decode_as(encoded_string, CharacterCode.UTF8)
+        self.assertEqual(decoded_string, string_to_encode)
+
+    def test_encode_decode_english(self):
+        string_to_encode = "Hello, World!"  # 英語文字列
+        encoded_string = self.encode_as(string_to_encode, CharacterCode.UTF8)
+        decoded_string = self.decode_as(encoded_string, CharacterCode.UTF8)
+        self.assertEqual(decoded_string, string_to_encode)
+
+    def test_encode_decode_indonesian(self):
+        string_to_encode = "Halo, Dunia!"  # インドネシア語文字列
+        encoded_string = self.encode_as(string_to_encode, CharacterCode.UTF8)
+        decoded_string = self.decode_as(encoded_string, CharacterCode.UTF8)
+        self.assertEqual(decoded_string, string_to_encode)
+
+
+if __name__ == '__main__':
+    unittest.main()
