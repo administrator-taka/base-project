a7b61ed2644e99ba16b177d910c6ab684d678e1c
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-27 21:32:26 +0900

    動作確認用sql追記

--- django-project/myproject/resource/test.sql
+++ django-project/myproject/resource/test.sql
@@ -1,3 +1,4 @@
+--手動生成字幕一括取得
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
 ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
@@ -6,9 +7,19 @@ ORDER BY
 video_subtitle.subtitle_id ASC,
 video_subtitle.t_start_ms ASC;
 
+--指定した字幕で取得
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
 ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
 WHERE video_subtitle_info.subtitle_id = ''
 ORDER BY
 video_subtitle.t_start_ms ASC;
+
+--翻訳と時間が一致しているかの確認
+SELECT * FROM public.video_subtitle
+JOIN video_subtitle_info
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+WHERE video_subtitle_info.subtitle_type = 1
+ORDER BY
+video_subtitle.t_start_ms ASC,
+video_subtitle.subtitle_id DESC;
