b9ae31f3a54c4211cd23aafee16695f37538d4d8
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-27 18:39:57 +0900

    分割

--- django-project/myproject/myapp/views/health_check_view.py
+++ django-project/myproject/myapp/views/health_check_view.py
@@ -10,11 +10,14 @@ from myproject.settings.base import TEST_YOUTUBE_VIDEO_ID
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def health_check(request):
+    test_1()
+    return JsonResponse(data={"msg": "pass"}, status=200)
+
+
+def test_1():
     youtube_download_service = YoutubeDownloadService()
     video_id = TEST_YOUTUBE_VIDEO_ID
     default_audio_language = YouTubeLanguage.KOREAN
     translation_language = YouTubeLanguage.JAPANESE
 
     youtube_download_service.download_video_subtitle(video_id, default_audio_language, translation_language)
-
-    return JsonResponse(data={"msg": "pass"}, status=200)
