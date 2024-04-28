7533a95611dd4a9d0a8c5a3582c09baf368d6188
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-28 16:18:28 +0900

    ミリ秒変換処理修正

--- django-project/myproject/myapp/applications/util/util_convert.py
+++ django-project/myproject/myapp/applications/util/util_convert.py
@@ -1,4 +1,11 @@
 def convert_to_milliseconds(time_str):
-    h, m, s = time_str.split(':')
-    return int(h) * 3600000 + int(m) * 60000 + int(float(s) * 1000)
-
+    try:
+        # 時間文字列を時、分、秒、ミリ秒に分割
+        h, m, s_ms = time_str.split(':')
+        s, ms = s_ms.split('.')
+        # 時間、分、秒、ミリ秒を整数に変換してミリ秒に変換
+        milliseconds = int(h) * 3600000 + int(m) * 60000 + int(s) * 1000 + int(ms)
+        return milliseconds
+    except:
+        # 変換できない場合は None を返す
+        return None
