d8382ddfa6c175348fb8c1fbb17ce8f9935f3170
taka <124072973+administrator-taka@users.noreply.github.com>
2024-04-29 20:13:26 +0900

    テスト用sql修正

--- django-project/myproject/resource/test.sql
+++ django-project/myproject/resource/test.sql
@@ -1,16 +1,16 @@
 --手動生成字幕一括取得
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
 WHERE video_subtitle_info.subtitle_type = 1
 ORDER BY
-video_subtitle.subtitle_id ASC,
+video_subtitle.subtitle_info_id ASC,
 video_subtitle.t_start_ms ASC;
 
 --指定した字幕で取得
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
 WHERE video_subtitle_info.subtitle_id = ''
 ORDER BY
 video_subtitle.t_start_ms ASC;
@@ -18,20 +18,26 @@ video_subtitle.t_start_ms ASC;
 --翻訳と時間が一致しているかの確認
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
 WHERE video_subtitle_info.subtitle_type = 1
 ORDER BY
 video_subtitle.t_start_ms ASC,
-video_subtitle.subtitle_id DESC;
+video_subtitle.subtitle_info_id DESC;
 
 --完全一致
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
 WHERE video_subtitle.subtitle_text='모르겠는데';
 
 --部分一致
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
 WHERE video_subtitle.subtitle_text LIKE '%모르겠는데%';
+
+--出現度ランキング
+SELECT subtitle_text, COUNT(*) AS subtitle_count
+FROM video_subtitle
+GROUP BY subtitle_text
+ORDER BY subtitle_count DESC;
