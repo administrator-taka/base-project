aeb0cbcea627e506fd7d640d2f1388283033b2ed
taka <124072973+administrator-taka@users.noreply.github.com>
2024-05-17 20:11:51 +0900

    チャンネルページに遷移

--- vue-project/my-vue-app/src/components/youtube-app/YouTubeAppHome.vue
+++ vue-project/my-vue-app/src/components/youtube-app/YouTubeAppHome.vue
@@ -3,19 +3,29 @@
     <Sidebar />
     <main class="main-content">
       <div>YouTubeアプリケーションホーム画面</div>
+      <button @click="goToChannelPage">チャンネルページへ</button>
     </main>
   </div>
 </template>
 
 <script>
 import Sidebar from '@/components/SidebarComponent.vue'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
   components: {
     Sidebar
   },
   setup() {
-    return {}
+    const channelId = ref('UCHE7GBQVtdh-c1m3tjFdevQ')
+    const router = useRouter()
+
+    const goToChannelPage = () => {
+      router.push(`/channel/${channelId.value}`)
+    }
+
+    return { channelId, goToChannelPage }
   }
 }
 </script>
