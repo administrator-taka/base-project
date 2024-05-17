b6ed7ca0b3a36c07dee94030181d21eb2bcb57d0
taka <124072973+administrator-taka@users.noreply.github.com>
2024-05-18 01:56:28 +0900

    チャンネル一覧追記

--- vue-project/my-vue-app/src/components/youtube-app/YouTubeAppHome.vue
+++ vue-project/my-vue-app/src/components/youtube-app/YouTubeAppHome.vue
@@ -3,7 +3,13 @@
     <Sidebar />
     <main class="main-content">
       <h1>YouTubeアプリケーションホーム画面</h1>
-      <button @click="goToChannelPage">チャンネルページへ</button>
+      <ul>
+        <li v-for="(channelId, index) in channelIds" :key="index">
+          <button @click="goToChannelPage(channelId)">
+            チャンネルページへ {{ channelId }}
+          </button>
+        </li>
+      </ul>
     </main>
   </div>
 </template>
@@ -18,14 +24,27 @@ export default {
     Sidebar
   },
   setup() {
-    const channelId = ref('UCHE7GBQVtdh-c1m3tjFdevQ')
+    const channelIds = ref([
+      // 아이네
+      'UCroM00J2ahCN6k-0-oAiDxg',
+      // 징버거
+      'UCHE7GBQVtdh-c1m3tjFdevQ',
+      // 릴파
+      'UC-oCJP9t47v7-DmsnmXV38Q',
+      // 주르르
+      'UCTifMx1ONpElK5x6B4ng8eg',
+      // 고세구
+      'UCV9WL7sW6_KjanYkUUaIDfQ',
+      // 비챤
+      'UCs6EwgxKLY9GG4QNUrP5hoQ'
+    ])
     const router = useRouter()
 
-    const goToChannelPage = () => {
-      router.push(`/channel/${channelId.value}`)
+    const goToChannelPage = (channelId: string) => {
+      router.push(`/channel/${channelId}`)
     }
 
-    return { channelId, goToChannelPage }
+    return { channelIds, goToChannelPage }
   }
 }
 </script>
