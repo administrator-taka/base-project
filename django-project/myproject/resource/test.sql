a742ca67f93bb0926ae5c04784d3cb09057157a4
taka <124072973+administrator-taka@users.noreply.github.com>
2024-05-23 21:35:28 +0900

    test.sql修正

--- django-project/myproject/resource/test.sql
+++ django-project/myproject/resource/test.sql
@@ -1,56 +1,70 @@
 --手動生成字幕一括取得
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
 WHERE video_subtitle_info.subtitle_type = 1
 ORDER BY
-video_subtitle.subtitle_info_id ASC,
-video_subtitle.t_start_ms ASC;
+video_subtitle.subtitle_id ASC,
+video_subtitle.t_start_ms ASC
+LIMIT 100
+;
 
 --指定した字幕で取得
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
 WHERE video_subtitle_info.subtitle_id = ''
 ORDER BY
-video_subtitle.t_start_ms ASC;
+video_subtitle.t_start_ms ASC
+LIMIT 100
+;
 
 --翻訳と時間が一致しているかの確認
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
 WHERE video_subtitle_info.subtitle_type = 1
 ORDER BY
 video_subtitle.t_start_ms ASC,
-video_subtitle.subtitle_info_id DESC;
+video_subtitle.subtitle_id DESC
+LIMIT 100
+;
 
 --完全一致
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
-WHERE video_subtitle.subtitle_text='모르겠는데';
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+WHERE video_subtitle.subtitle_text='모르겠는데'
+LIMIT 100
+;
 
 --部分一致
 SELECT * FROM public.video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
-WHERE video_subtitle.subtitle_text LIKE '%모르겠는데%';
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
+WHERE video_subtitle.subtitle_text LIKE '%모르겠는데%'
+LIMIT 100
+;
 
 --出現度ランキング
 SELECT subtitle_text, COUNT(*) AS subtitle_count
 FROM video_subtitle
 GROUP BY subtitle_text
-ORDER BY subtitle_count DESC;
+ORDER BY subtitle_count DESC
+LIMIT 100
+;
 
 --出現度ランキング（言語指定、自動指定：手動だと歌詞などで不自然な集計が行われるため）
 SELECT subtitle_text, COUNT(*) AS subtitle_count
 FROM video_subtitle
 JOIN video_subtitle_info
-ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_info_id
+ON video_subtitle_info.subtitle_id = video_subtitle.subtitle_id
 WHERE video_subtitle_info.language_code='ko'
 AND subtitle_type=0
 GROUP BY subtitle_text
-ORDER BY subtitle_count DESC;
+ORDER BY subtitle_count DESC
+LIMIT 100
+;
 
 --初期データ
 "UCHE7GBQVtdh-c1m3tjFdevQ","ko","{ja}"
@@ -74,7 +88,8 @@ where video_subtitle_info.subtitle_type=1
 and video_subtitle_info.video_id='MMt-p8w-h8c'
 order by video_subtitle.t_start_ms,
 video_subtitle_info.language_code desc
-
+LIMIT 100
+;
 --検索
 select * from video_detail
 join video_subtitle_info
@@ -88,3 +103,4 @@ video_subtitle_info.video_id,
 video_subtitle.t_start_ms,
 video_subtitle.t_offset_ms
 LIMIT 100
+;
