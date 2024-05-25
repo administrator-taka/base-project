9833d407b1b2e693249411d06e9483d89de795fe
taka <124072973+administrator-taka@users.noreply.github.com>
2024-05-25 16:42:00 +0900

    テーブルデザイン修正

--- vue-project/my-vue-app/src/components/common/table/JsonTable.vue
+++ vue-project/my-vue-app/src/components/common/table/JsonTable.vue
@@ -9,7 +9,26 @@
     <tbody>
       <tr v-for="(value, key) in data" :key="key">
         <td>{{ key }}</td>
-        <td>{{ value }}</td>
+        <td>
+          <div v-if="Array.isArray(value)">
+            <table v-if="isArrayOfObjects(value)" class="table table-sm">
+              <tr>
+                <th v-for="(val, k) in value[0]" :key="k">{{ k }}</th>
+              </tr>
+              <tr v-for="(val, index) in value" :key="index">
+                <td v-for="(item, k) in val" :key="k">{{ item }}</td>
+              </tr>
+            </table>
+            <div v-else>
+              <span v-for="(item, index) in value" :key="index">{{
+                item
+              }}</span>
+            </div>
+          </div>
+          <div v-else>
+            {{ value }}
+          </div>
+        </td>
       </tr>
     </tbody>
   </table>
@@ -23,8 +42,30 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    isArrayOfObjects(value) {
+      return (
+        Array.isArray(value) && value.every((item) => typeof item === 'object')
+      )
+    }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.table-primary {
+  background-color: #d1ecf1;
+  color: #0c5460;
+}
+
+.table-primary th,
+.table-primary td {
+  border-color: #bee5eb;
+}
+
+.table-primary thead th {
+  background-color: #0c5460;
+  color: white;
+}
+</style>
