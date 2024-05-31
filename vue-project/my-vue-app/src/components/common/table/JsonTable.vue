68a0c3f6255da41de37a4799db87dca4e454377c
taka <124072973+administrator-taka@users.noreply.github.com>
2024-05-31 23:14:46 +0900

    テーブルデザイン修正

--- vue-project/my-vue-app/src/components/common/table/JsonTable.vue
+++ vue-project/my-vue-app/src/components/common/table/JsonTable.vue
@@ -5,24 +5,20 @@
         <table v-if="data" class="table">
           <tbody>
             <tr v-for="(value, key) in data" :key="key">
-              <th>{{ key }}</th>
+              <th :style="{ width: `${getKeyWidth(key)}px` }">{{ key }}</th>
               <td style="word-break: break-word">
                 <div v-if="Array.isArray(value)">
                   <table v-if="isArrayOfObjects(value)" class="table table-sm">
-                    <th>
+                    <tbody>
                       <tr v-for="(val, k) in value[0]" :key="k">
-                        {{
-                          k
-                        }}
+                        <th :style="{ width: `${getKeyWidth(k)}px` }">
+                          {{ k }}
+                        </th>
+                        <td v-for="(innerValue, index) in value" :key="index">
+                          {{ innerValue[k] }}
+                        </td>
                       </tr>
-                    </th>
-                    <td v-for="(val, index) in value" :key="index">
-                      <tr v-for="(item, k) in val" :key="k">
-                        {{
-                          item
-                        }}
-                      </tr>
-                    </td>
+                    </tbody>
                   </table>
                   <div v-else>
                     <span v-for="(item, index) in value" :key="index">{{
@@ -44,7 +40,6 @@
 
 <script>
 export default {
-  name: 'JsonTable',
   props: {
     data: {
       type: Object,
@@ -56,9 +51,12 @@ export default {
       return (
         Array.isArray(value) && value.every((item) => typeof item === 'object')
       )
+    },
+    getKeyWidth(key) {
+      // Calculate the width based on the length of the key
+      const width = key.length * 10 + 20 // Adjust the multiplier as needed
+      return width
     }
   }
 }
 </script>
-
-<style scoped></style>
