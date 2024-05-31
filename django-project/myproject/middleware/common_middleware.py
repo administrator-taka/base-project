dd7ab5851b057c2b8f55a80dc8a9bc67f03cf910
taka <124072973+administrator-taka@users.noreply.github.com>
2024-05-31 14:34:22 +0900

    フォーマッタ、マイグレーション

--- django-project/myproject/middleware/common_middleware.py
+++ django-project/myproject/middleware/common_middleware.py
@@ -2,6 +2,7 @@ import logging
 
 from django.utils.deprecation import MiddlewareMixin
 
+
 class CommonMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, *view_args, **view_kwargs):
         logging.debug("★★★：通ったことの確認")
@@ -14,7 +15,7 @@ class CommonMiddleware(MiddlewareMixin):
             # 以下は例としてユーザー名を取得するダミーの処理
             username = self.extract_username_from_token(token)
             request.username = username
-            logging.debug("ユーザー名："+username)
+            logging.debug("ユーザー名：" + username)
         else:
             # Authorizationヘッダーが存在しない場合やBearerトークンが使用されていない場合は、Noneを設定する
             request.username = None
