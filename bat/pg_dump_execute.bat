dcd33b40506415b0477462a7e6227d75da294d34
taka <124072973+administrator-taka@users.noreply.github.com>
2024-06-11 01:11:34 +0900

    db更新に確認処理を追加

--- bat/pg_dump_execute.bat
+++ bat/pg_dump_execute.bat
@@ -1,3 +1,13 @@
+@echo off
+chcp 65001 >nul
+set /p CONTINUE=本当に実行しますか？ (y/n):
+
+if /i "%CONTINUE%" NEQ "y" (
+    echo 操作はキャンセルされました。
+    goto :EOF
+)
+
 docker exec -i postgresql psql -U postgres -d postgres -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 docker exec -i postgresql psql -U postgres -d postgres < test.sql
+
 @REM cmd /k
