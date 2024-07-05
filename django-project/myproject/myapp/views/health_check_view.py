2f2a0429018c0347b7db469ae6e9fb5ece97ef5f
taka <124072973+administrator-taka@users.noreply.github.com>
2024-07-05 15:51:30 +0900

    削除用

--- django-project/myproject/myapp/views/health_check_view.py
+++ django-project/myproject/myapp/views/health_check_view.py
@@ -2,8 +2,13 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
+from myapp.applications.application.service.channel_service import ChannelService
+
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def health_check(request):
+    youtube_download_service = ChannelService()
+    channel_id = ''
+    youtube_download_service.delete_channel_data(channel_id)
     return JsonResponse(data={"msg": "pass"}, status=200)
