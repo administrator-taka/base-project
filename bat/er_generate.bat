583d86a8b96528eced5e63290c5eec062e7df551
taka <124072973+administrator-taka@users.noreply.github.com>
2024-07-05 11:17:32 +0900

    ER図生成bat作成

---
+++ bat/er_generate.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM SchemaSpyを実行してスキーマ情報を出力
+docker run -v "%cd%/schema:/output" --net="host" schemaspy/schemaspy:6.1.0 -t pgsql -host localhost:5432 -db postgres -u postgres -p postgres -connprops useSSL\=false -all
+
+cmd /k
