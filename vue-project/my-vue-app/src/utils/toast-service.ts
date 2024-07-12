d2d4fd288a504e853b13de024515497e16ab7e3a
taka <124072973+administrator-taka@users.noreply.github.com>
2024-07-12 13:54:06 +0900

    フォーマッタ

--- vue-project/my-vue-app/src/utils/toast-service.ts
+++ vue-project/my-vue-app/src/utils/toast-service.ts
@@ -2,13 +2,15 @@ import { useToast } from 'vue-toastification'
 
 const toast = useToast()
 
-export const showErrorToast = (error: { response: { data: { message: string; }; } }) => {
+export const showErrorToast = (error: {
+  response: { data: { message: string } }
+}) => {
   const errorMessage = error.response.data.message
   toast.error(`エラーが発生しました。詳細: ${errorMessage}`, {
     timeout: 5000,
     closeOnClick: true,
     pauseOnHover: true
-  });
+  })
 }
 
 export const showSuccessToast = (message: string) => {
