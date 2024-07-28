8952bd01c05ce8f8997a56958a2cdff8a1071a10
taka <124072973+administrator-taka@users.noreply.github.com>
2024-07-28 17:17:44 +0900

    動画ID抽出

--- vue-project/my-vue-app/src/components/TopComponent.vue
+++ vue-project/my-vue-app/src/components/TopComponent.vue
@@ -2,13 +2,16 @@
   <div>
     <main class="main-content">
       <div>トップ画面（仮）</div>
+      <SelectVideo></SelectVideo>
     </main>
   </div>
 </template>
 
 <script lang="ts">
+import SelectVideo from './youtube-app/video/SelectVideo.vue'
+
 export default {
-  components: {}
+  components: { SelectVideo }
 }
 </script>
 
---
+++ vue-project/my-vue-app/src/components/youtube-app/video/SelectVideo.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h2>YouTube動画IDとタイムスタンプ抽出器</h2>
+    <div class="col-md-6">
+      <input
+        v-model="youtubeUrl"
+        type="text"
+        class="form-control"
+        placeholder="YouTube動画のURLを入力"
+        required
+      />
+    </div>
+    <div v-if="videoId">
+      <h3>抽出された動画ID</h3>
+      <p>{{ videoId }}</p>
+      <button @click="goToVideoPage(videoId)" class="btn btn-primary m-2">
+        動画ページへ
+      </button>
+
+      <h3 v-if="timestamp !== null">抽出されたタイムスタンプ</h3>
+      <p v-if="timestamp !== null">{{ timestamp }} 秒</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, watch, defineComponent } from 'vue'
+import router from '@/router'
+
+export default defineComponent({
+  setup() {
+    const youtubeUrl = ref('')
+    const videoId = ref('')
+    const timestamp = ref<number | null>(null)
+
+    const extractVideoDetails = () => {
+      const url = youtubeUrl.value
+
+      // YouTube動画IDを抽出する正規表現
+      const videoIdMatch = url.match(
+        /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/|v\/|shorts\/|user\/(?:[^#&?]*).*))([^#&?]*)/
+      )
+      videoId.value =
+        videoIdMatch && videoIdMatch[1].length === 11 ? videoIdMatch[1] : ''
+
+      // タイムスタンプを抽出する正規表現
+      const timestampMatch = url.match(/[?&]t=(\d+)/)
+      timestamp.value = timestampMatch ? parseInt(timestampMatch[1], 10) : null
+
+      if (videoId.value) {
+        console.log(`取得した動画ID: ${videoId.value}`)
+      } else {
+        console.error('動画IDが取得できませんでした')
+      }
+
+      if (timestamp.value !== null) {
+        console.log(`取得したタイムスタンプ: ${timestamp.value} 秒`)
+      } else {
+        console.log('タイムスタンプがありません')
+      }
+    }
+
+    const goToVideoPage = (videoId: string) => {
+      router.push(`/video/${videoId}`)
+    }
+
+    // youtubeUrlが変更されるたびにextractVideoDetailsを実行
+    watch(youtubeUrl, extractVideoDetails)
+
+    return {
+      youtubeUrl,
+      videoId,
+      timestamp,
+      goToVideoPage
+    }
+  }
+})
+</script>
+
+<style scoped>
+/* スタイルを追加 */
+</style>
